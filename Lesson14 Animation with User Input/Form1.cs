3896194a6b9125ef46a91862afb5cd7d7f8d0d0c
Cynthia Stone <cmstone24@hotmail.com>
2025-11-30 11:29:21 -0500

    Add project files.

---
+++ Lesson14 Animation with User Input/Form1.cs
@@ -0,0 +1,15 @@
+namespace Lesson14_Animation_with_User_Input
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
